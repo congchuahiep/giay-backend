a93fdc2938acf6d3bc8b2a8155dcd0f117436d7e
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2026-06-19 21:47:47 +0700

    feat(migration): add workspace tables and rename migration

--- migration/src/m20220101_000001_create_tables.rs
+++ migration/src/m20220101_000001_create_tables.rs
@@ -46,12 +46,63 @@ impl MigrationTrait for Migration {
         )
         .await?;
 
+        db.execute_unprepared(
+            /* sql */
+            r#"
+            CREATE TABLE workspace (
+                id UUID PRIMARY KEY,
+                name TEXT NOT NULL,
+                slug TEXT UNIQUE NOT NULL,
+                icon TEXT NULL,
+                owner_id UUID REFERENCES "user"(id) ON DELETE CASCADE,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+            );
+            "#,
+        )
+        .await?;
+
+        db.execute_unprepared(
+            /* sql */
+            r#"
+            CREATE TYPE workspace_role AS ENUM ('owner', 'moderator', 'member', 'viewer');
+            "#,
+        )
+        .await?;
+
+        db.execute_unprepared(
+            /* sql */
+            r#"
+            CREATE TABLE workspace_membership (
+                workspace_id UUID REFERENCES workspace(id) ON DELETE CASCADE,
+                user_id UUID REFERENCES "user"(id) ON DELETE CASCADE,
+                role workspace_role NOT NULL,
+                joined_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+                PRIMARY KEY (workspace_id, user_id)
+            );
+            "#,
+        )
+        .await?;
+
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_type(Type::drop().name("user_role").to_owned())
+            .drop_table(Table::drop().table("workspace_membership").to_owned())
+            .await?;
+
+        manager
+            .drop_type(Type::drop().name("workspace_role").to_owned())
+            .await?;
+
+        manager
+            .drop_table(Table::drop().table("workspace").to_owned())
+            .await?;
+
+        manager
+            .drop_table(Table::drop().table("user_session").to_owned())
             .await?;
 
         manager
@@ -59,7 +110,7 @@ impl MigrationTrait for Migration {
             .await?;
 
         manager
-            .drop_table(Table::drop().table("user_session").to_owned())
+            .drop_type(Type::drop().name("user_role").to_owned())
             .await?;
 
         Ok(())
